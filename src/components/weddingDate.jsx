bec80f0e40b4bc967ae25c9a07187d57f2f0f652
Jorge Andres <jorgeandresherreramonsalve@gmail.com>
2025-07-06 15:05:07 -0500

    Cambiando fecha de boda

--- src/components/weddingDate.jsx
+++ src/components/weddingDate.jsx
@@ -1,37 +1,36 @@
-import { ScheduleButton } from "./scheduleButton"
+import { ScheduleButton } from "./scheduleButton";
 
 export function WeddingDate() {
-    return(
-        <>
-            <div className="text-center my-5 wedding-date">
-                <h5 className="fw-light text-uppercase month">Junio</h5>
+  return (
+    <>
+      <div className="text-center my-5 wedding-date">
+        <h5 className="fw-light text-uppercase month">Julio</h5>
 
-                <div className="row align-items-center justify-content-center">
-                    <div className="col-3 text-end">
-                        <div className="gold-line"></div>
-                        <div className="fw-bold text-uppercase side-text">Sábado</div>
-                    </div>
+        <div className="row align-items-center justify-content-center">
+          <div className="col-3 text-end">
+            <div className="gold-line"></div>
+            <div className="fw-bold text-uppercase side-text">Sábado</div>
+          </div>
 
-                    <div className="col-3 display-3 fw-bold main-number">14</div>
+          <div className="col-3 display-3 fw-bold main-number">26</div>
 
-                    <div className="col-3 text-start">
-                        <div className="gold-line"></div>
-                        <div className="fw-bold side-text">2025</div>
-                    </div>
-                </div>
-            </div>
-            <div className="row align-items-center justify-content-center">
-                <ScheduleButton />
-            </div>
-            
+          <div className="col-3 text-start">
+            <div className="gold-line"></div>
+            <div className="fw-bold side-text">2025</div>
+          </div>
+        </div>
+      </div>
+      <div className="row align-items-center justify-content-center">
+        <ScheduleButton />
+      </div>
 
-            {/* Google Fonts */}
-            <link
-                href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@500;700&family=Libre+Baskerville&display=swap"
-                rel="stylesheet"
-            />
+      {/* Google Fonts */}
+      <link
+        href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@500;700&family=Libre+Baskerville&display=swap"
+        rel="stylesheet"
+      />
 
-            <style>{`
+      <style>{`
                 .gold-line {
                 height: 2px;
                 background: linear-gradient(to right, #d4af37, #d4af37);
@@ -58,6 +57,6 @@ export function WeddingDate() {
                 font-size: 4rem;
                 }
             `}</style>
-        </>
-    )
+    </>
+  );
 }
